8a4b2cf689485c6fdd734a4da2f6ced5392a336d
BDDoylan <bddoylan@gmail.com>
2021-06-22 18:51:07 -0400

    Initial commit with Home.js component made

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <img src="http://wp.widewallpapers.net/4k/money/2560x1440/money-2560x1440-003.jpg" alt="bank"/>
+            <h1>Bank of React</h1>    
+        </div>
+    )
+}
+
+export default Home
