8429158f4ac2cf9af8156fdd3cfa18b704bd2011
BDDoylan <bddoylan@gmail.com>
2021-06-27 14:21:26 -0400

    Improving css for all current components.

---
+++ src/components/AccountBalance.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AccountBalance extends Component {
+  render() {
+    return <div className='balance font-link'>Balance: {this.props.accountBalance}</div>;
+  }
+}
+
+export default AccountBalance;
